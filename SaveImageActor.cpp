851ca10ec1128abacd6398f5192a5e4870b05f2d
susanni <shan99ni@gmail.com>
2021-02-08 15:37:17 -0500

    Async code does not build. Something related to ENQUEUE_RENDER_COMMAND. Will look into later.

--- SaveImageActor.cpp
+++ SaveImageActor.cpp
@@ -6,8 +6,7 @@
 #include "IImageWrapper.h"
 #include "IImageWrapperModule.h"
 
-
-// Sets default values
+// Sets default values.
 ASaveImageActor::ASaveImageActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
@@ -15,7 +14,7 @@ ASaveImageActor::ASaveImageActor()
 
 }
 
-// Called when the game starts or when spawned
+// Called when the game starts or when spawned.
 void ASaveImageActor::BeginPlay()
 {
 	Super::BeginPlay();
@@ -25,15 +24,18 @@ void ASaveImageActor::BeginPlay()
 	}
 }
 
-// Called every frame
+// Called every frame.
 void ASaveImageActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	++ticks;
+	++ticks_;
 
-	if (ticks%30 == 0) {
+	if (ticks_%30 == 0) {
+		if (DisableSaving) return;
 		SaveImage();
+		// // Start an async task to save color image to disk so the game thread is not blocked (which drops the FPS).
+		// (new FAutoDeleteAsyncTask<Image2Png2DiskAsyncTask>(this, "color_" + FString::FromInt(ticks_) + ".png"))->StartBackgroundTask();
 	}
 }
 
@@ -43,7 +45,7 @@ void ASaveImageActor::SaveImage(const FString& FilePath, const FString& ColorNam
 	int32 Width = TextureTarget->SizeX;
 	int32 Height = TextureTarget->SizeY;
 
-	CaptureComponent->CaptureScene();
+	// CaptureComponent->CaptureScene(); // Should not be used if bCaptureEveryFrame enabled.
 
 	TArray<FColor> Image;
 	Image.AddZeroed(Width * Height);
@@ -54,14 +56,12 @@ void ASaveImageActor::SaveImage(const FString& FilePath, const FString& ColorNam
 	TextureTarget->GameThread_GetRenderTargetResource()->ReadPixels(Image, ReadSurfaceDataFlags);
 
 	TArray<uint8> ImgData = Image2Png(Image, Width, Height);
-	FFileHelper::SaveArrayToFile(ImgData, *(FilePath + ColorName + FString::FromInt(ticks)));
+	FFileHelper::SaveArrayToFile(ImgData, *(FilePath + ColorName + FString::FromInt(ticks_) + ".png"));
 }
 
 TArray<uint8> ASaveImageActor::Image2Png(const TArray<FColor>& Image, int32 Width, int32 Height) {
 	if (Image.Num() == 0 || Image.Num() != Width * Height) {
-		if(GEngine) {
-			GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Yellow, FString::Printf(TEXT("Tick %d: Image.Num() == %d not equal Width * Height (%d * %d). Returning empty image."), ticks, Image.Num(), Width, Height));
-		}
+		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Yellow, FString::Printf(TEXT("Tick %d: Image.Num() == %d not equal Width * Height (%d * %d). Returning empty image."), ticks_, Image.Num(), Width, Height));
 		return TArray<uint8>();
 	}
 
@@ -72,4 +72,67 @@ TArray<uint8> ASaveImageActor::Image2Png(const TArray<FColor>& Image, int32 Widt
 	TArray<uint8> ImgData;  // avoiding error: no known conversion from 'const TArray64<uint8>' (aka 'const TArray<unsigned char, TSizedDefaultAllocator<64> >') to 'std::initializer_list<unsigned char>' for 1st argument
 	ImgData = ImageWrapper->GetCompressed();
 	return ImgData;
+}
+
+// bool ASaveImageActor::ReadPixels(TArray<FColor>& OutImageData, FReadSurfaceDataFlags InFlags) {
+// 	FTextureRenderTarget2DResource* RenderResource = (FTextureRenderTarget2DResource*) TextureTarget->Resource;
+	
+// 	// Read the render target surface data back.
+// 	struct FReadSurfaceContext {
+// 		FRenderTarget* SrcRenderTarget;
+// 		TArray<FColor>* OutData;
+// 		FIntRect Rect;
+// 		FReadSurfaceDataFlags Flags;
+// 	};
+
+// 	OutImageData.Reset();
+
+// 	FReadSurfaceContext Context =
+// 	{
+// 		RenderResource,
+// 		&OutImageData,
+// 		FIntRect(0, 0, RenderResource->GetSizeXY().X, RenderResource->GetSizeXY().Y),
+// 		// InFlags
+// 		FReadSurfaceDataFlags(RCM_UNorm, CubeFace_MAX)
+// 	};
+
+// 	ENQUEUE_RENDER_COMMAND(ReadSurfaceCommand)(
+// 		[Context](FRHICommandListImmediate& RHICmdList)
+// 		{
+// 			RHICmdList.ReadSurfaceData(
+// 				Context.SrcRenderTarget->GetRenderTargetTexture(),
+// 				Context.Rect,
+// 				*Context.OutData,
+// 				Context.Flags
+// 			);
+// 		});
+// 	return OutImageData.Num() > 0;
+// }
+
+
+Image2Png2DiskAsyncTask::Image2Png2DiskAsyncTask(ASaveImageActor* SaveImageActor, const FString& FileName, const FString& FilePath) {
+	SaveImageActor_ = SaveImageActor;
+	EntireFilePath_ = FilePath + FileName;
+}
+
+void Image2Png2DiskAsyncTask::DoWork() {
+	int32 Width = SaveImageActor_->TextureTarget->SizeX;
+	int32 Height = SaveImageActor_->TextureTarget->SizeY;
+
+	// SaveImageActor_->CaptureComponent->CaptureScene();  // Should not be used if bCaptureEveryFrame enabled.
+
+	TArray<FColor> Image;
+	Image.AddZeroed(Width * Height);
+	
+	GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Green, FString::Printf(TEXT("%s"), *EntireFilePath_));
+
+	FReadSurfaceDataFlags ReadSurfaceDataFlags;
+	ReadSurfaceDataFlags.SetLinearToGamma(false);
+
+	if (SaveImageActor_->TextureTarget->GameThread_GetRenderTargetResource()->ReadPixels(Image, ReadSurfaceDataFlags)) {
+		TArray<uint8> ImgData = SaveImageActor_->Image2Png(Image, Width, Height);
+		FFileHelper::SaveArrayToFile(ImgData, *EntireFilePath_);
+	} else {
+		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, FString::Printf(TEXT("FAILED TO READ PIXELS ON TEXTURE TARGET.")));	
+	}
 }
--- SaveImageActor.h
+++ SaveImageActor.h
@@ -4,7 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Containers/UnrealString.h"
-#include "Engine.h"  // USceneCaptureComponent2D
+#include "Engine.h"  // Contains includes for almost all Unreal headers, i.e. USceneCaptureComponent2D.
 #include "GameFramework/Actor.h"
 #include "SaveImageActor.generated.h"
 
@@ -15,16 +15,26 @@ class PED_API ASaveImageActor : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
+	// Sets default values for this actor's properties.
 	ASaveImageActor();
 
 protected:
-	// Called when the game starts or when spawned
+	// Called when the game starts or when spawned.
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
+	// Called every frame.
 	virtual void Tick(float DeltaTime) override;
+
+    /**
+	 * Converts raw image data to compressed PNG image data.
+	 * @param Image  Raw image data.
+	 * @param Width  Width of image. (i.e. number of rows)
+	 * @param Height Height of image. (i.e. number of columns)
+	 * References:
+	 * https://github.com/unrealcv/unrealcv/blob/master/Source/UnrealCV/Private/Serialization.cpp#L67
+	 */
+	TArray<uint8> Image2Png(const TArray<FColor>& Image, int32 Width, int32 Height);
 
 	/**
 	 * Writes color and depth images to disk.
@@ -34,15 +44,50 @@ public:
 	 */
 	void SaveImage(const FString& FilePath = "/home/swarm/Downloads/Images/", const FString& ColorName = "color_", const FString& DepthName = "depth_");
 
-    /**
-	 * TODO(sni): Document.
+	/**
+	 * Replacing RenderTarget::ReadPixels() because FlushRenderingCommands() has to be run
+	 * in the game thread and blocks it, which causes FPS to drop.
+	 * References:
+	 * https://michaeljcole.github.io/wiki.unrealengine.com/Render_Target_Lookup/
+	 * https://github.com/EpicGames/UnrealEngine/blob/release/Engine/Source/Runtime/Engine/Private/UnrealClient.cpp#L49
 	 */
-	TArray<uint8> Image2Png(const TArray<FColor>& Image, int32 Width, int32 Height);
+	// bool ReadPixels(TArray<FColor>& OutImageData, FReadSurfaceDataFlags InFlags);
 
 	UPROPERTY(EditAnywhere) USceneCaptureComponent2D* CaptureComponent;
 	UPROPERTY(EditAnywhere) UTextureRenderTarget2D* TextureTarget;
 	UPROPERTY(EditAnywhere) bool DisableSaving;  // If true, don't save images to disk.
 
 private:
-	int64 ticks;  // count of number of ticks that have passed since game started.
+	int64 ticks_;  // Count of number of ticks that have passed since game started.
+};
+
+
+/**
+ * Task to asynchronously save a PNG color image to disk such that ReadPixels doesn't block the game thread.
+ * References:
+ * https://github.com/unrealcv/unrealcv/blob/master/Source/UnrealCV/Private/GTCaptureComponent.cpp#L228
+ * https://github.com/TimmHess/UnrealImageCapture/blob/master/CaptureToDisk/Source/CaptureToDisk/Private/CaptureManager.cpp#L225
+ */
+class Image2Png2DiskAsyncTask : public FNonAbandonableTask {
+public:
+	/**
+	 * Constructs async task.
+	 * @param SaveImageActor Entire instance to access capture component and render target.
+	 * @param FileName Name to save image to.
+	 * @param FilePath Path to directory of where image should be save.
+	 */
+	Image2Png2DiskAsyncTask(ASaveImageActor* SaveImageActor, const FString& FileName, const FString& FilePath = "/home/swarm/Downloads/Images/");
+	~Image2Png2DiskAsyncTask(){}
+
+	// Required by UE4!
+    FORCEINLINE TStatId GetStatId() const{
+        RETURN_QUICK_DECLARE_CYCLE_STAT(AsyncSaveImageToDiskTask, STATGROUP_ThreadPoolAsyncTasks);
+    }
+
+	// Executes save PNG color image to disk task.
+	void DoWork();
+
+private:
+	ASaveImageActor* SaveImageActor_;  // Used to access capture component and render target.
+	FString EntireFilePath_;  // Path to save image including image name.
 };
